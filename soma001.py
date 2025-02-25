0da257230914ec0e5b3ae920bf5c7ae9eeedd1b3
MayaraBinsfeld <mayara.binsfeld@gmail.com>
2025-02-25 20:27:03 -0300

    Add files via upload

---
+++ soma001.py
@@ -0,0 +1,25 @@
+"""
+Programa soma
+Descrição:
+Este programa soma os números 2 e 3.
+Autor: Mayara de Almeida Binsfeld
+Versão: 0.0.1
+Data: 25/02/2025
+"""
+# Alocação de memória 
+
+numero_1 = 2
+numero_2 = 3
+
+
+# Entrada de dados
+
+
+
+# Processamento de dados
+
+soma = numero_1 + numero_2
+
+# Saida de dados
+
+print(soma)
